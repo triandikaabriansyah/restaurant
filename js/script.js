f3d388207c6fda2e127a2a926ce230584dc9b8cd
triandikaabriansyah <triandikaabriansyah@gmail.com>
2021-06-03 11:39:47 +0700

    Ubah data json

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function tampilSemuaMenu(){
 $('#daftar-menu').html('');
-$.getJSON('data/pizza.json', function(data){
+$.getJSON('data/chicken.json', function(data){
     let menu = data.menu;
     $.each(menu, function(i, data){
         $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama +'</h5><p class="card-text">'  + data.deskripsi + '</p><h5 class="card-title">Rp. ' + data.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
@@ -22,7 +22,7 @@ $('.nav-link').on('click', function(){
         return;
     }
 
-    $.getJSON('data/pizza.json', function (data){
+    $.getJSON('data/chicken.json', function (data){
         let menu = data.menu;
         let content = '';
 
